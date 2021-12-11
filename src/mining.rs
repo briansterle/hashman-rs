eda62c2fe0ba626062a338497e549e291103a6e6
Brian Sterle <sterle.brian@gmail.com>
2021-12-11 12:41:05 -0500

    RigState(bool); refactoring due to rust being mad

--- src/mining.rs
+++ src/mining.rs
@@ -1,13 +1,16 @@
+use std::borrow::Borrow;
 use std::process::Command;
+use sysinfo::{Process, ProcessExt, Signal, SystemExt};
 use crate::Config;
-use crate::gpu::{GPU, WindowsGPU};
+use crate::gpu::{GPU, GPULoad, WindowsGPU};
 use crate::rig::{Rig, RigProcess, RigState};
+
+use sysinfo::{System};
 
 pub struct Mining;
 
 
 impl Mining {
-
     pub fn restart(config: &Config) -> Result<RigState, RigState> {
         let output = Command::new(&config.miner_exe)
             .output()
@@ -15,24 +18,41 @@ impl Mining {
 
         match output.status.code() {
             Some(code) if code == 0 => {
-                Ok(RigState::Mining(None))
+                Ok(RigState::Mining(false))
             }
             Some(code) => {
                 println!("Unexpectedly exited mining exe with status code: {}", code);
-                Err(RigState::Idle(None))
+                Err(RigState::Idle(false))
             }
-            None => Err(RigState::Idle(None))
+            None => Err(RigState::Idle(false))
         }
     }
 
-    pub fn is_healthy(gpu: &WindowsGPU) -> bool {
-        match gpu.get_util() {
-            Ok(util) => { util.load > 0.5 && Self::is_process_running() }
-            Err(_) => { false }
-        }
-    }
+    pub fn run(gpu: &WindowsGPU) -> RigState {
+        match (sysinfo::System::new_all().processes().values().find(|p|
+                    p.name().to_lowercase().contains("nicehash")),
+               gpu.get_util()
+        ) {
+            (Some(p), Ok(u)) if (u.load > 0.5) => {
+                RigState::Mining(false)
+            }
+            (Some(p), Ok(u)) if (u.load <= 0.5) => {
+                RigState::Mining(p.kill(Signal::Kill))
+            }
+            (Some(p), Err(e)) => {
+                RigState::Idle(p.kill(Signal::Kill))
+            }
+            (None, Ok(u)) => if u.load > 0.5 {
+                RigState::Gaming(false)
+            } else {
+                RigState::Idle(false)
+            }
 
-    pub fn is_process_running() -> bool {
-        Rig::filter_processes("nicehash") > 0
+            (None, Err(e)) => {
+                RigState::Idle(false)
+            }
+
+            _ => unreachable!()
+        }
     }
 }
--- src/rig.rs
+++ src/rig.rs
@@ -1,16 +1,16 @@
+use std::borrow::Borrow;
 use sysinfo;
-use sysinfo::{Process, ProcessExt, SystemExt};
+use sysinfo::{Process, ProcessExt, System, SystemExt};
 
 use crate::{Config, GPU, WindowsGPU};
 use crate::mining::Mining;
 use crate::rig::RigState::Idle;
 
 #[derive(Debug)]
-// #[derive(PartialEq)]
 pub enum RigState {
-    Idle(Option<Process>),
-    Mining(Option<Process>),
-    Gaming(Option<Process>),
+    Idle(bool),
+    Mining(bool),
+    Gaming(bool),
 }
 
 impl PartialEq for RigState {
@@ -23,7 +23,7 @@ pub struct Rig;
 
 impl Rig {
     pub fn update_state(current: RigState, config: &Config) -> RigState {
-        if current == RigState::Idle(None) {
+        if current == RigState::Idle(false) {
             Mining::restart(config).expect("oops")
         } else {
             current
@@ -31,22 +31,6 @@ impl Rig {
     }
 
     pub fn current_state(gpu: &WindowsGPU) -> RigState {
-        if Mining::is_healthy(gpu) {
-            RigState::Mining(None)
-        } else if gpu.get_util().expect("err running gputil.py").load > 0.5 {
-            RigState::Gaming(None)
-        } else {
-            RigState::Idle(None)
-        }
-    }
-}
-
-impl RigProcess for Rig {}
-
-pub trait RigProcess {
-    fn filter_processes(str: &str) -> usize {
-        sysinfo::System::new_all().processes().values()
-            .filter(|p| p.name().to_lowercase().contains(&str.to_lowercase()))
-            .count()
+        Mining::run(gpu)
     }
 }
