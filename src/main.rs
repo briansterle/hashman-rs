0a066c929245fa92e21c3e72ddf0eca81bbf2564
Brian Sterle <sterle.brian@gmail.com>
2021-12-16 18:59:11 -0500

    Rig::

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ use hashman_rs::{HashEnv, Rig};
 
 fn main() {
   let updated: Rig = HashEnv::setup().run();
-  println!("Hashman [INFO] rig::move_state = {:?}", updated);
+  println!("Hashman [INFO] Rig::move_state = {:?}", updated);
 }
