dad27e6683ce05e47b2f21aea814d0eab9e41997
Brian Sterle <sterle.brian@gmail.com>
2021-12-17 12:28:55 -0500

    main optionally loops

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
+use std::env;
+use std::time::Duration;
+
 use hashman_rs::{HashEnv, Rig};
 
 fn main() {
-  let updated: Rig = HashEnv::setup().run();
-  println!("Hashman [INFO] Rig::move_state = {:?}", updated);
+  let loops: u64 = match env::args().collect().get(1) {
+    None => 1,
+    Some(count) => count.parse::<u64>().unwrap(),
+  };
+  let mut i = 0;
+  while i < loops {
+    i += 1;
+    let updated: Rig = HashEnv::setup().run();
+    println!("Hashman [INFO] Rig::move_state = {:?}", updated);
+    std::thread::sleep(Duration::from_secs(1));
+  }
 }
