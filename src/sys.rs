5af0522fdd4d594bbea37e12b93fe2e9a6bd6526
Brian Sterle <sterle.brian@gmail.com>
2021-12-18 10:53:58 -0500

    fix inverted gaming/mining paths

--- src/sys.rs
+++ src/sys.rs
@@ -28,10 +28,10 @@ impl Sys {
 
     // initial search for gaming and mining parent processes
     for (pid, p) in processes {
-      if hash_path.mining_path.contains(&p.name().to_owned()) {
+      if hash_path.gaming_path.contains(&p.name().to_owned()) {
         println!("{}", Self::pretty_proc(p, "Gaming Process"));
         p1.push(pid.to_owned());
-      } else if hash_path.gaming_path.contains(&p.name().to_owned()) {
+      } else if hash_path.mining_path.contains(&p.name().to_owned()) {
         println!("{}", Self::pretty_proc(p, "Mining Process"));
         p2.push(pid.to_owned());
       }
@@ -41,10 +41,10 @@ impl Sys {
     for (pid, p) in self.refresh().system.processes() {
       if let Some(parent) = p.parent() {
         if p1.contains(&parent) {
-          println!("Found a p1 child: {}", pid);
+          println!("Gaming child: {}", pid);
           p1.push(pid.to_owned());
         } else if p2.contains(&parent) {
-          println!("Found a p2 child: {}", pid);
+          println!("Mining child: {}", pid);
           p2.push(pid.to_owned());
         }
       }
