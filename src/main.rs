66acc6b71877096a8e49850cf7aa96238f478e05
Brian Sterle <sterle.brian@gmail.com>
2021-12-20 13:48:20 -0500

    sugar for parsing CLI args into various types

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,12 @@ use hashman_rs::{get_arg_or, HashEnv, Rig};
 
 fn main() {
   env_logger::init();
-
-  let args = std::env::args().collect::<Vec<String>>();
-
-  let loops = get_arg_or(1, |s| s.parse::<u64>().expect("loops: int"), 21_000_000);
-
-  let interval = get_arg_or(
+  let loops: u64 = get_arg_or(1, |s| s.parse::<u64>().expect("loops: int"), 21_000_000);
+  let interval: Duration = get_arg_or(
     2,
     |sec| Duration::from_secs(sec.parse::<u64>().expect("duration: sec")),
     Duration::from_secs(21),
   );
-
   let mut env = HashEnv::setup();
   for _ in 1..loops {
     let updated: Rig = env.run();
